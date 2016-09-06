4a1eaf3773b7309401e44f1d4e055a8431097485
limingmin <314407103@qq.com>
2016-09-06 15:22:21 +0800

    初始项目

---
+++ index.js
@@ -0,0 +1,19 @@
+import loaderUtils from 'loader-utils'
+import miniCSS from 'mini-css'
+
+var classReg = /(\S+)node_modules\/@sdp.nd\/([\w|-]+)\/(widget|page)-([\w|-]+)\/(\S+)/g
+module.exports = function (content) {
+  this.cacheable && this.cacheable()
+  var widgetUrl = loaderUtils.getRemainingRequest(this)
+  if (classReg.test(widgetUrl)) {
+    var className = widgetUrl.replace(/(\S+)node_modules\/@sdp.nd\/([\w|-]+)\/(widget|page)-([\w|-]+)\/(\S+)/g, '$2-$4')
+    var result = []
+    // 删除无关的字符串
+    miniCSS(content).split('}').forEach(text => {
+      text && result.push(`.${className} ${text}}`)
+    })
+    return result.join('')
+  } else {
+    return content
+  }
+}
