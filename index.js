2e53703d93605f313cfe77cc71f6b0113ada0fea
limingmin <314407103@qq.com>
2016-09-06 21:18:40 +0800

    update

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var loaderUtils = require('loader-utils')
 var miniCSS = require('mini-css')
 
-var classReg = /(\S+)node_modules\/@sdp.nd\/([\w|-]+)\/(widget|page)-([\w|-]+)\/(\S+)/g
+var classReg = /(\S+)node_modules\/@sdp.nd\/([\w|-]+)\/(widget)-([\w|-]+)\/(\S+)/g
 module.exports = function (content) {
-  this.cacheable && this.cacheable()
   var widgetUrl = loaderUtils.getRemainingRequest(this)
   if (classReg.test(widgetUrl)) {
-    var className = widgetUrl.replace(/(\S+)node_modules\/@sdp.nd\/([\w|-]+)\/(widget|page)-([\w|-]+)\/(\S+)/g, '$2-$4')
+    var className = widgetUrl.replace(classReg, '$2-$4')
     var result = []
     // 删除无关的字符串
     miniCSS(content).split('}').forEach(text => {
