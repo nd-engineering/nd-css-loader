8fb01b46d8868b247eadf508b07c43d5b953b444
limingmin <314407103@qq.com>
2016-09-19 16:01:17 +0800

    已post css 的方式嵌套样式

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var loaderUtils = require('loader-utils')
 
-var classReg = /(\S+)node_modules\/@sdp.nd\/([\w|-]+)\/(widget)-([\w|-]+)\/(\S+)/g
+var classReg = /(\S+)node_modules\/@sdp.nd\/([\w|-]+)\/(widget-[\w|-]+)\/(\S+)/g
 module.exports = function (content) {
   var widgetUrl = loaderUtils.getRemainingRequest(this)
   if (classReg.test(widgetUrl)) {
-    var className = widgetUrl.replace(classReg, '$2-$4')
+    var className = widgetUrl.replace(classReg, '$2-$3')
     return `.${className} { ${content} }`
   } else {
     return content
