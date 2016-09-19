31aa89bfdc5e880b1b9628fd6da39f767c8a3bd9
limingmin <314407103@qq.com>
2016-09-19 14:08:56 +0800

    已post css 的方式嵌套样式

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 var loaderUtils = require('loader-utils')
-var miniCSS = require('mini-css')
 
 var classReg = /(\S+)node_modules\/@sdp.nd\/([\w|-]+)\/(widget)-([\w|-]+)\/(\S+)/g
 module.exports = function (content) {
   var widgetUrl = loaderUtils.getRemainingRequest(this)
   if (classReg.test(widgetUrl)) {
     var className = widgetUrl.replace(classReg, '$2-$4')
-    var result = []
-    // 删除无关的字符串
-    miniCSS(content).split('}').forEach(text => {
-      text && result.push(`.${className} ${text}}`)
-    })
-    return result.join('')
+    return `.${className} { ${content} }`
   } else {
     return content
   }
