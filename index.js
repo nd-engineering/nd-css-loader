542bccbc23f9719ce0f1c5d6847e35d0ea232965
limingmin <314407103@qq.com>
2016-09-20 10:00:40 +0800

    修改 CSS 嵌套规则

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var loaderUtils = require('loader-utils')
-
+var css = require('css')
+// 不要匹配 page 因为page要控制 HTML BODY
 var classReg = /(\S+)node_modules\/@sdp.nd\/([\w|-]+)\/(widget-[\w|-]+)\/(\S+)/g
 module.exports = function (content) {
   var widgetUrl = loaderUtils.getRemainingRequest(this)
   if (classReg.test(widgetUrl)) {
     var className = widgetUrl.replace(classReg, '$2-$3')
-    return `.${className} { ${content} }`
+    var ast = css.parse(content) // .stylesheet.rules[0].selectors.join('|')
+    try {
+      ast.stylesheet && ast.stylesheet.rules && ast.stylesheet.rules.forEach(rule => {
+        rule.selectors && rule.selectors.forEach((selector, i) => {
+          rule.selectors[i] = `.${className} ${selector}`
+        })
+      })
+    } catch (e) {
+      console.log('nd-css-loader 解析异常')
+      console.log(e)
+    }
+    // console.log(css.stringify(ast))
+    return content
   } else {
     return content
   }
