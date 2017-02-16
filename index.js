b07a1c7c9558d74f628d732eee0113d34591d755
limingmin <314407103@qq.com>
2017-02-16 22:43:47 +0800

    1.修复 window 下的目录匹配问题 2.修复 解析 POSTCSS 问题

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 var loaderUtils = require('loader-utils')
-var css = require('css')
+var postcss = require('postcss')
 // 不要匹配 page 因为page要控制 HTML BODY
-var classReg = /(\S+)node_modules\/@sdp.nd\/([\w|-]+)\/(widget-[\w|-]+)\/(\S+)/g
+var classReg = /(\S+)node_modules[\/\\]@sdp.nd[\/\\]([\w|-]+)[\/\\](widget-[\w|-]+)[\/\\](\S+)/g
 // 要找出手淘的前缀 [data-dpr="\d"]
 var flexibleReg = /^\s*(\[data-dpr\s*=\s*["']\d+["']\])(.+)$/
 module.exports = function (content) {
   var widgetUrl = loaderUtils.getRemainingRequest(this)
   if (classReg.test(widgetUrl)) {
     var className = widgetUrl.replace(classReg, '$2-$3')
-    var ast = css.parse(content) // .stylesheet.rules[0].selectors.join('|')
+    var ast = postcss.parse(content) // .stylesheet.rules[0].selectors.join('|')
     try {
-      ast.stylesheet && ast.stylesheet.rules && ast.stylesheet.rules.forEach(rule => {
-        rule.selectors && rule.selectors.forEach((selector, i) => {
-          var selectorMatch = selector.match(flexibleReg)
+      ast.nodes.forEach(rule => {
+        if (rule.type === 'rule' && rule.selector !== ':root') {
+          var selectorMatch = rule.selector.match(flexibleReg)
           if (selectorMatch && selectorMatch.length === 3) {
-            rule.selectors[i] = `${selectorMatch[1]} .${className} ${selectorMatch[2]}` // [data-dpr="\d"] .className .selector
+            rule.selector = `${selectorMatch[1]} .${className} ${selectorMatch[2]}` // [data-dpr="\d"] .className .selector
           } else {
-            rule.selectors[i] = `.${className} ${selector}`
+            rule.selector = `.${className} ${rule.selector}`
           }
-        })
+        }
       })
     } catch (e) {
       console.log('nd-css-loader 解析异常')
       console.log(e)
     }
     // console.log(css.stringify(ast))
-    return css.stringify(ast)
+    return ast.toResult().css
   } else {
     return content
   }
